d93e9348f63ed033463a6b6ed3b6b679684b6244
Dusousas <dusousadeveloper@gmail.com>
2025-05-20 13:46:54 -0300

    link

--- components/subc/Navbar.tsx
+++ components/subc/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useState } from 'react';
+import Link from 'next/link';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 export default function Navbar() {
@@ -15,10 +16,10 @@ export default function Navbar() {
             {/* Desktop Navigation */}
             <nav className='hidden lg:block'>
                 <ul className='flex gap-6 font-Jost text-lg font-semibold text-BTN'>
-                    <li><a href="/">Início</a></li>
-                    <li><a href="#about">Sobre</a></li>
-                    <li><a href="/imoveis">Imóveis</a></li>
-                    <li><a href="#contact">Contato</a></li>
+                    <li><Link href="/">Início</Link></li>
+                    <li><Link href="#about">Sobre</Link></li>
+                    <li><Link href="/imoveis">Imóveis</Link></li>
+                    <li><Link href="#contact">Contato</Link></li>
                 </ul>
             </nav>
 
@@ -32,7 +33,7 @@ export default function Navbar() {
                     {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
                 </button>
 
-                {/* Mobile Menu Fullscreen - opens from right to left */}
+                {/* Mobile Menu Fullscreen */}
                 <nav className={`fixed top-0 right-0 h-full w-full bg-white z-50 transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
                     <div className="flex justify-end p-4">
                         <button 
@@ -46,10 +47,10 @@ export default function Navbar() {
                     
                     <div className="flex items-center justify-center h-full">
                         <ul className='flex flex-col gap-8 font-Jost text-2xl font-semibold text-BTN text-center'>
-                            <li><a href="/" onClick={toggleMenu}>Início</a></li>
-                            <li><a href="#about" onClick={toggleMenu}>Sobre</a></li>
-                            <li><a href="/imoveis" onClick={toggleMenu}>Imóveis</a></li>
-                            <li><a href="#contact" onClick={toggleMenu}>Contato</a></li>
+                            <li><Link href="/" onClick={toggleMenu}>Início</Link></li>
+                            <li><Link href="#about" onClick={toggleMenu}>Sobre</Link></li>
+                            <li><Link href="/imoveis" onClick={toggleMenu}>Imóveis</Link></li>
+                            <li><Link href="#contact" onClick={toggleMenu}>Contato</Link></li>
                         </ul>
                     </div>
                 </nav>
